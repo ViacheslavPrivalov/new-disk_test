2a8902cefab1faa996cf6bdcc93518c5a59afc4a
Viacheslav Privalov <sprivalov98@mail.ru>
2024-05-22 10:33:42 +0300

    made first configuration of project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { LessonsModule } from './lessons/lessons.module';
+import { EvaluationsModule } from './evaluations/evaluations.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '**/**/*.entity{.js, .ts}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    LessonsModule,
+    EvaluationsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const cfg = app.get(ConfigService);
+  const PORT = cfg.get('PORT') || 8080;
+
+  await app.listen(PORT, () =>
+    console.log(`Server is running on port ${PORT}`),
+  );
 }
 bootstrap();
