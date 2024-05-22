b06f355d20b2a5414b69a9032b6b4eea2458aa0b
Viacheslav Privalov <sprivalov98@mail.ru>
2024-05-22 18:01:39 +0300

    add swagger and simple validation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,17 @@ async function bootstrap() {
   const PORT = cfg.get('PORT') || 8080;
 
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Backend тестовое в Новый Диск')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,18 +2,28 @@ import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserDto } from './dto/user.dto';
 
+@ApiTags('Пользователи')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Получение списка пользователей' })
+  @ApiResponse({ status: 200, description: 'OK', type: [UserDto] })
   async getUsers() {
     return await this.userService.getUsers();
   }
 
   @Post()
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Создание пользователя' })
+  @ApiResponse({ status: 201, description: 'Created', type: UserDto })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 404, description: 'Not found' })
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.userService.createUser(createUserDto);
   }
